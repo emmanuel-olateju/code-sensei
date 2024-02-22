c2f7df06708f681a4cd31f07fbc4b8d350918822
emmanuel-olateju <olatejuemmanuel@gmail.com>
2024-02-22 02:53:19 +0100

    chat framework completed

--- code-sensei.py
+++ code-sensei.py
@@ -6,6 +6,13 @@ import openai
 
 openai.api_key = os.getenv("CODE_SENSEI_OPENAI_KEY")
 
+def generate_code_and_explanation(prompt):
+    response = openai.chat.completions.create(
+        model='gpt-3.5-turbo',
+        messages=prompt
+    )
+    return  response.choices[0].message.content
+
 def set_chat_history():
     pass
 
@@ -23,7 +30,8 @@ if 'selected_chat_history' not in st.session_state:
 
 if st.session_state.selected_chat_history==None:
     if 'chat_history' not in st.session_state:
-        st.session_state.chat_history = list()
+        st.session_state.chat_history = [[],[],[],[]]
+        st.session_state.messages = []
     else:
         st.session_state.selected_chat_history = 'New Chat'
 else:
@@ -45,11 +53,15 @@ def signup():
         st.session_state.education = st.selectbox('Education Level',['','Primary','Secondary','Undergraduate','Post-graduate'])
         st.session_state.track = st.selectbox('Preferred Track',['','Software Engineering','Data Science','DevOps','Cyber Security'])
         st.session_state.user_language = st.selectbox('Primary Language',['','Python','C#','C++','C','Javascript','Pseudocode'])
+        #Temprorary
+        st.session_state.user_language = 'python'
         col1, col2, col3 = st.columns([0.33]*3)
         with col2:
             st.button('Sign Up',on_click=chatting,key='sign_up')
 
 def login():
+    #Temprorary
+    st.session_state.user_language = 'python'
     login_sidebar = st.sidebar
     with login_sidebar:
         st.session_state.email = st.text_input('email',key='login_email')
@@ -86,7 +98,7 @@ def main():
         with st.sidebar:
             st.button('New Tinker')
             st.session_state.selected_chat_history = option_menu('Old chats',st.session_state.past_chats)
-        st.session_state.chat_input = st.chat_input('Paste Your Code Snippet Here')
+        st.session_state.chat_input = st.chat_input('What do you want your code to do?')
         if st.session_state.chat_just_starting==True:
             cols = st.columns([0.2]+([0.1]*8))
             st.container(height=170,border=False)
@@ -95,41 +107,46 @@ def main():
                 cols = st.columns([0.3,0.3,0.2])
                 with cols[1]:
                     st.image('6.jpg',width=100)
-                cols = st.columns([0.2,0.8,0.2])
+                cols = st.columns([0.03,0.93,0.04])
                 with cols[1]:
-                    st.markdown('#### I\'d love to help you interprete codes faster')
+                    st.markdown('#### I\'d love to help you learn and interprete codes faster')
             st.session_state.chat_just_starting = False
             print('Here')
         else:
             print('chat_started')
             if st.session_state.chat_input!=None:
-                st.session_state.chat_history.append({
+                st.session_state.messages.append(st.session_state.chat_input)
+                prompt = f'Write a {st.session_state.user_language} code that {st.session_state.chat_input} and then explain it'
+                st.session_state.chat_history[0].append({
                     'role':'user',
-                    'content':st.session_state.chat_input
+                    'content':prompt
                 })
-                code_response = openai.chat.completions.create(
-                    model='gpt-3.5-turbo',
-                    messages=st.session_state.chat_history
-                )
-                # code_explaination = "Testing Explaination"
-                st.session_state.chat_history.apapend({
+                code_snippet = generate_code_and_explanation(st.session_state.chat_history[0])
+                st.session_state.chat_history[1].append({
                     'role':'ai',
-                    'content':code_response.choices[0].message.content
+                    'content':code_snippet
                 })
-                # st.session_state.chat_history.apapend({
-                #     'role':'ai',
-                #     'content':code_explaination
-                # })
-                st.session_state.chat_history.append({})
+
+                prompt = f'In an explicit manner towards a 3 year old child, explain what each line of the code below does \n {code_snippet} \
+                with reference to the line number of each  line in the code'
+                st.session_state.chat_history[2].append({
+                    'role':'user',
+                    'content':prompt
+                })
+                code_explaination = generate_code_and_explanation(st.session_state.chat_history[2])
+                st.session_state.chat_history[3].append({
+                    'role':'ai',
+                    'content':code_explaination
+                })
+                
                 print('length of chat history:{}'.format(len(st.session_state.chat_history)))
-                for message in st.session_state.chat_history[:-1]:
+                for message_index,message in enumerate(st.session_state.chat_history[0]):
                     with st.chat_message(message['role']):
-                        st.markdown(message['content'])
-                    with st.chat_message('role'):
-                        st.code(message['content'])
-                    # with st.chat_message('ai'):
-                    #     st.markdown(message['ai_'])
-        # print(len(st.session_state.chat_history))
+                        st.markdown(st.session_state.messages[message_index])
+                    with st.chat_message(st.session_state.chat_history[1][message_index]['role']):
+                        st.code(st.session_state.chat_history[1][message_index]['content'],language=st.session_state.user_language,line_numbers=True)
+                        st.markdown(st.session_state.chat_history[3][message_index]['content'])
+
 
 if __name__=='__main__':
     main()
